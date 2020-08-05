8d509bcd735c6a820cdf2731dad9ce3064d35a5c
SAITO Atsushi <maldikulo@gmail.com>
2020-08-05 22:12:33 +0900

    Added option of compile only. Display the program whose label has been resolved.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 extern crate aaron_asm;
 
 use aaron_asm::*;
@@ -25,18 +24,28 @@ where
 }
 
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
+    let mut arg_p: usize = 1;
+    let mut compile_only = false;
     if args.len() > 1 {
-        let file = File::open(&args[1]);
+        if args[arg_p] == "-c" {
+            compile_only = true;
+            arg_p += 1;
+        }
+        let file = File::open(&args[arg_p]);
         let mut file = file.if_error_then_exit();
         let mut contents = String::new();
         file.read_to_string(&mut contents).unwrap();
         let program = contents.parse();
         let program = program.if_error_then_exit();
-        let stdout = std::io::stdout();
-        let mut handle = stdout.lock();
-        let mut machine = MachineState::new(&mut handle);
-        println!("{}", machine.run(&program));
+        if compile_only {
+            print!("{}", program);
+        } else {
+            let stdout = std::io::stdout();
+            let mut handle = stdout.lock();
+            let mut machine = MachineState::new(&mut handle);
+            println!("{}", machine.run(&program));
+        }
     } else {
         eprintln!("Command line argument is invalid");
         std::process::exit(3);
